3670b8a4f6c18a9c791143857547b60e3879ffe2
Mikhail Dordevic <mb@data-screen.com>
2019-12-25 18:23:10 +0300

    Add :)

---
+++ install.sh
@@ -0,0 +1,14 @@
+cd backend && npm i
+echo 'Backend installed!'
+
+cd ../frontend && npm i
+echo 'Frontend installed!'
+
+cd ../services/admin && npm i
+echo 'Admin service installed!'
+
+cd ../passport && npm i
+echo 'Passport service installed!'
+
+cd ../uploader && npm i
+echo 'Uploader service installed!'
