e14331b13db005d8320d2920a53f3883d8815dd9
Mikhail Dordevic <mb@data-screen.com>
2019-12-27 12:16:48 +0300

    Здравствуйте, влагалищи

--- ecosystem.dev.config.js
+++ ecosystem.dev.config.js
@@ -12,10 +12,7 @@ ecosystem.apps.push(
     max_memory_restart: '1G',
     env: {
       PORT: 4001,
-      BROWSER: 'none',
       NODE_ENV: 'development',
-      REACT_APP_SERVICE_AUTH_LOGIN_ENDPOINT: 'http://localhost:4003/login',
-      REACT_APP_SERVICE_AUTH_SIGNUP_ENDPOINT: 'http://localhost:4003/signup',
     },
   },
 );
--- services/passport/src/server.js
+++ services/passport/src/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.set('host', '0.0.0.0');
 app.set('port', process.env.PORT || 8080);
 app.use(express.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 app.use(passport.initialize());
 app.use(passport.session());
