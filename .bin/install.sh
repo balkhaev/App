1e54184cc3d81097eebff02d56f4451cbfa5c921
Mikhail Dordevic <mb@data-screen.com>
2019-12-28 04:16:00 +0300

    Formatting messages and install sendi service

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,27 +1,30 @@
 cd backend
 npm i
-echo 'Backend installed!'
+echo '===== Backend installed! ====='
 
 cd admin
 npm i
-echo 'Backend Admin installed!'
+echo '===== Backend Admin installed! ====='
 npm run build
-echo 'Backend Admin builded!'
+echo '===== Backend Admin builded! ====='
 
 cd ../../frontend
 npm i
-echo 'Frontend installed!'
+echo '===== Frontend installed! ====='
 npm run build
-echo 'Frontend builded!'
+echo '===== Frontend builded! ====='
 
 cd ../services/passport
 npm i
-echo 'Passport service installed!'
 [ -f .env ] || cp .env.example .env # copy example .env file if production .env not exists
-echo 'Passport service envs copied!'
+echo '===== Passport service installed! ====='
 
 cd ../uploader
 npm i
-echo 'Uploader service installed!'
 [ -f .env ] || cp .env.example .env
-echo 'Uploader service envs copied!'
+echo '===== Uploader service installed! ====='
+
+cd ../sendi
+npm i
+[ -f .env ] || cp .env.example .env
+echo '===== Sendi service installed! ====='
