2c10b7bfbe76996f454caecf098ef9c5494e6749
Mikhail Dordevic <mb@data-screen.com>
2019-12-28 09:31:10 +0300

    FileUploader component fixes

--- frontend/components/FileUploader/FileUploader.js
+++ frontend/components/FileUploader/FileUploader.js
@@ -3,10 +3,10 @@ import { Button } from '@material-ui/core';
 
 const FileUploader = () => {
   const handleInputChange = e => {
-    var file = e.target.files[0];
+    const file = e.target.files[0];
 
-    var upload = new tus.Upload(file, {
-      endpoint: 'http://localhost:1080/files/',
+    const upload = new tus.Upload(file, {
+      endpoint: '/api/upload',
       retryDelays: [0, 3000, 5000, 10000, 20000],
       metadata: {
         filename: file.name,
@@ -16,7 +16,8 @@ const FileUploader = () => {
         console.log('Failed because: ' + error);
       },
       onProgress: function(bytesUploaded, bytesTotal) {
-        var percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2);
+        const percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2);
+
         console.log(bytesUploaded, bytesTotal, percentage + '%');
       },
       onSuccess: function() {
@@ -24,12 +25,21 @@ const FileUploader = () => {
       },
     });
 
+    e.target.value = '';
+
     upload.start();
-  }
+  };
 
   return (
     <div>
-      <input id="contained-button-file" onChange={handleInputChange} accept="image/*" style={{ display: 'none' }} multiple type="file" />
+      <input
+        id="contained-button-file"
+        onChange={handleInputChange}
+        accept="image/*"
+        style={{ display: 'none' }}
+        multiple
+        type="file"
+      />
       <label htmlFor="contained-button-file">
         <Button variant="contained" component="span">
           Upload
