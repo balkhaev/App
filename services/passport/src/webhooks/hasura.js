a6a679896d26e50fe20148886b833f7e2d339555
Mikhail Dordevic <mb@data-screen.com>
2019-12-28 13:19:34 +0300

    Fix hasura webhook

--- services/passport/src/webhooks/hasura.js
+++ services/passport/src/webhooks/hasura.js
@@ -1,4 +1,6 @@
-module.exports = (passport, { hasuraUa = 'hasura-graphql-engine/v1.0.0', adminRole = 'admin', userRole, anonymousRole } = {}) => {
+module.exports = (passport, opts = {}) => {
+  const { adminRole = 'admin', userRole, anonymousRole } = opts;
+
   if (typeof anonymousRole !== 'string') {
     throw new Error('Anonymous role required!');
   }
@@ -8,33 +10,56 @@ module.exports = (passport, { hasuraUa = 'hasura-graphql-engine/v1.0.0', adminRo
   }
 
   return async (req, res, next) => {
-    const { userAgent, xHasuraRole = adminRole } = req.headers;
+    const {
+      authorization,
+      'user-agent': userAgent,
+      'sec-fetch-mode': secFetchMode,
+      'x-hasura-role': xHasuraRole = adminRole,
+    } = req.headers;
 
-    // Hasura admin interface webhook auth check
-    if (userAgent === hasuraUa) {
+    // Hasura admin interface
+    if (secFetchMode === 'cors' && userAgent.includes('hasura-graphql-engine/')) {
       return res.json({
         'X-Hasura-Role': xHasuraRole,
       });
     }
 
-    passport.authenticate('bearer', async (error, user) => {
+    if (!authorization) {
+      return res.json({
+        'X-Hasura-Role': anonymousRole,
+      });
+    }
+
+    const auth = passport.authenticate('bearer', async (error, user) => {
       if (error) {
         return res.status(401).json({ error });
       }
 
-      if (user) {
-        const { id, company_id, role = userRole } = await user.getUser();
-
-        res.json({
-          'X-Hasura-User-Company-Id': `${company_id}`,
-          'X-Hasura-User-Id': `${id}`,
-          'X-Hasura-Role': `${role}`,
-        });
-      } else {
-        res.json({
+      if (!user) {
+        return res.json({
           'X-Hasura-Role': anonymousRole,
         });
       }
-    })(req, res, next);
+
+      /**
+       * Схема такая:
+       * - Если есть пользователь, но нету роли - role === 'user'
+       * - Если есть пользователь и есть роль - role === user.role
+       * - Если есть пользователь и он админ - role === (x-hasura-role || user.role)
+       */
+      let { id, role = userRole, company_id } = await user.getUser();
+
+      if (role === adminRole) {
+        role = xHasuraRole;
+      }
+
+      res.json({
+        'X-Hasura-User-Company-Id': `${company_id}`,
+        'X-Hasura-User-Id': `${id}`,
+        'X-Hasura-Role': `${role}`,
+      });
+    });
+
+    auth(req, res, next);
   };
 };
