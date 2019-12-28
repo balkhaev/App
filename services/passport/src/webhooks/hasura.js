ece5c0593b8ad1e46286b2ff77ac947114ea4bb8
Mikhail Dordevic <mb@data-screen.com>
2019-12-28 14:15:34 +0300

    Add passport secret env and admin logging as users

--- services/passport/src/webhooks/hasura.js
+++ services/passport/src/webhooks/hasura.js
@@ -1,3 +1,36 @@
+const camelCaseToDash = str => str.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase();
+const toHeaders = (opts, prefix = '') => {
+  return Object.keys(opts).reduce((acc, key) => {
+    acc[prefix + camelCaseToDash(key)] = `${opts[key]}`;
+
+    return acc;
+  }, {});
+};
+const createHasuraHeaders = (role, userId, userCompanyId) =>
+  toHeaders({ role, userId, userCompanyId }, 'x-hasura-');
+
+/**
+ * Схема авторизации такая:
+ *
+ * - Если x-passport-secret заголовок совпадает с секретом в .env и user-agent содержит часть Hasura юзер агента
+ * - тогда - role === (x-hasura-role || "admin")
+ *
+ * - Если нету authorization заголовка
+ * - тогда - role === 'anonymous'
+ *
+ * - Если есть authorization заголовок, но нету пользователя
+ * - тогда - role === 'anonymous'
+ *
+ * - Если есть пользователь, но нету роли
+ * - тогда - role === 'user'
+ *
+ * - Если есть пользователь и есть роль
+ * - тогда - role === user.role
+ *
+ * - Если есть пользователь и он админ
+ * - тогда - role === (x-hasura-role || user.role)
+ */
+
 module.exports = (passport, opts = {}) => {
   const { adminRole = 'admin', userRole, anonymousRole } = opts;
 
@@ -13,53 +46,43 @@ module.exports = (passport, opts = {}) => {
     const {
       authorization,
       'user-agent': userAgent,
-      'sec-fetch-mode': secFetchMode,
+      'x-passport-pass': xPassportPass,
       'x-hasura-role': xHasuraRole = adminRole,
     } = req.headers;
 
     // Hasura admin interface
-    if (secFetchMode === 'cors' && userAgent.includes('hasura-graphql-engine/')) {
-      return res.json({
-        'X-Hasura-Role': xHasuraRole,
-      });
+    if (xPassportPass === process.env.SECRET && userAgent.includes('hasura-graphql-engine/')) {
+      return res.json(createHasuraHeaders(xHasuraRole));
     }
 
     if (!authorization) {
-      return res.json({
-        'X-Hasura-Role': anonymousRole,
-      });
+      return res.json(createHasuraHeaders(anonymousRole));
     }
 
-    const auth = passport.authenticate('bearer', async (error, user) => {
+    passport.authenticate('bearer', async (error, user) => {
       if (error) {
         return res.status(401).json({ error });
       }
 
       if (!user) {
-        return res.json({
-          'X-Hasura-Role': anonymousRole,
-        });
+        return res.json(createHasuraHeaders(anonymousRole));
       }
 
-      /**
-       * Схема такая:
-       * - Если есть пользователь, но нету роли - role === 'user'
-       * - Если есть пользователь и есть роль - role === user.role
-       * - Если есть пользователь и он админ - role === (x-hasura-role || user.role)
-       */
       let { id, role = userRole, company_id } = await user.getUser();
 
+      // Даем возможность админу посмотреть сайт за любого пользователя и компанию.
       if (role === adminRole) {
+        const {
+          'x-hasura-user-id': xHasuraUserId = id,
+          'x-hasura-user-company-id': xHasuraUserCompanyId = company_id,
+        } = req.headers;
+
+        id = xHasuraUserId;
         role = xHasuraRole;
+        company_id = xHasuraUserCompanyId;
       }
 
-      res.json({
-        'X-Hasura-User-Company-Id': `${company_id}`,
-        'X-Hasura-User-Id': `${id}`,
-        'X-Hasura-Role': `${role}`,
-      });
-    });
-
-    auth(req, res, next);
+      res.json(createHasuraHeaders(role, id, company_id));
+    })(req, res, next);
   };
 };
