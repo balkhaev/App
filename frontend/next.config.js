56c565667db6483f277be3a1688b7cf05f210e75
Mikhail Dordevic <mb@data-screen.com>
2019-12-29 21:36:18 +0300

    Add env to build next

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 const webpack = require('webpack');
 
 module.exports = {
+  env: {
+    GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
+  },
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(process.env));
 
