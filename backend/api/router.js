d957b5671d45a5af6063958eb379b1bb5dcb6389
Mikhail Dordevic <mb@data-screen.com>
2020-01-24 12:20:25 +0300

    Add key to app

--- backend/api/router.js
+++ backend/api/router.js
@@ -144,6 +144,10 @@ router.use(
     target: SPORTRECS_URL,
     pathRewrite: { '^/api/sportrecs/': '/' },
     changeOrigin: true,
+    headers: {
+      authorization:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZTIxOTc4ZDNmMDkyMTQyMDFjY2U2MzciLCJpYXQiOjE1NzkyNTk4MTR9.e_7Ge2EcLo-V0kcYZK90Hl09FQkOkcqpxEvsQ7oBRTo',
+    },
   })
 );
 
